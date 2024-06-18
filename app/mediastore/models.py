4ab049377c7dbcb0ba4a1dc3b5247416d1c763b9
Sidney <sbatchelder@whoi.edu>
2024-06-18 11:28:28 -0400

    ninja dto

--- app/mediastore/models.py
+++ app/mediastore/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Media(models.Model):
+    class MediaIDTypes(models.TextChoices):
+        AMPID = "AMP", _("Amplify Global ID")
+        S3_OBJ = "OBJ", _("Object Store ID")
+        IFCB_BIN = "BIN", _("IFCB Bin ID")
+        IFCB_BIN_IMG = "ROI", _("IFCB ROI ID")
+        CHECKSUM = "CHK", _("CHECKSUM")
+        FILENAME = "FLN", _("FILENAME")
+
+    pid = models.CharField(max_length=255)
+    pid_type = models.CharField(choices=MediaIDTypes)
+    metadata = models.JSONField()
+    # TODO lifecycle Tags, Version, other relationships, json field
+
+    def __str__(self):
+        return self.pid
+
+
--- app/mediastore/views.py
+++ app/mediastore/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from ninja import NinjaAPI
 
-# Create your views here.
+from mediastore.dto_models import MediaInput, MediaOutput, MediaService
+
+api = NinjaAPI()
+
+
+@api.get("/hello")
+def hello(request):
+    return {"message": "Hello, world!"}
+
+
+@api.post('/media', response=MediaOutput)
+def create_media(request, input: MediaInput):
+    return MediaService.create(input)
+
+
+@api.get('/media/{id}', response=MediaOutput)
+def read_media(request, id: int):
+    return MediaService.read(id)
+
+
+@api.put('/media/{id}')
+def update_media(request, id: int, input: MediaInput):
+    MediaService.update(id, input)
+    return 204
+
+
+@api.delete('/media/{id}')
+def delete_media(request, id: int):
+    MediaService.delete(id)
+    return 204
+
+
+@api.get('/media', response=list[MediaOutput])
+def list_media(request):
+    return MediaService.list()
