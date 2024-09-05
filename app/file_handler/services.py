459fda139ca5d9e347cea103c0e502725f141fd9
Sidney <sbatchelder@whoi.edu>
2024-09-05 10:03:15 -0400

    upload n download

--- app/file_handler/services.py
+++ app/file_handler/services.py
@@ -11,6 +11,7 @@ import storage  # amplify_storage_utils
 from file_handler.schemas import UploadSchemaInput, UploadSchemaOutput, UploadError, \
                                  DownloadSchemaInput, DownloadSchemaOutput, DownloadError
 from mediastore.services import MediaService
+from mediastore.models import StoreConfig
 
 
 class UploadService:
@@ -31,11 +32,33 @@ class UploadService:
 
     @staticmethod
     def upload_with_file(payload: UploadSchemaInput) -> UploadSchemaOutput|UploadError:
-        # TODO save the file with storage_util
+        mediadata = MediaService.create(payload.mediadata)  # returns MediaSchema after creating database entry
+        # TODO verify that store_key is properly set or generated
+        store_config = mediadata.store_config
+        match store_config.type:
+            case StoreConfig.FILESYSTEMSTORE:
+                with storage.fs.FilesystemStore(store_config.bucket) as store:
+                    store.put(mediadata.store_key, payload.file.read())
+            case StoreConfig.BUCKETSTORE:
+                s3_params = store_config.s3_params
+                s3_session = storage.s3.aiobotocore.session.get_session()
+                S3_CLIENT_ARGS = dict(
+                    endpoint_url = s3_params.url,
+                    aws_access_key_id = s3_params.access_key,
+                    aws_secret_access_key = s3_params.secret_key, # todo, is this always available?
+                )
+                with s3_session.create_client('s3', **S3_CLIENT_ARGS) as s3client:
+                    with storage.s3.BucketStore(s3client, store_config.bucket) as store:
+                        store.put(mediadata.store_key, payload.file.read())
+            case StoreConfig.SQLITESTORE:
+                with storage.db.SqliteStore(store_config.bucket) as store:
+                    store.put(mediadata.store_key, payload.file.read())
+
         return UploadSchemaOutput(object_url=f'the_ready_object_url pid={payload.mediadata.pid} fname={payload.file.name}')
 
     @staticmethod
     def upload_sans_file(payload: UploadSchemaInput) -> UploadSchemaOutput|UploadError:
+        assert payload.mediadata.store_config.type == StoreConfig.BUCKETSTORE
         # TODO generate presigned url with storage_util
         return UploadSchemaOutput(presigned_url='my_presigned_url')
 
@@ -56,10 +79,28 @@ class DownloadService:
 
     @staticmethod
     def download_direct(payload: DownloadSchemaInput) -> DownloadSchemaOutput|DownloadError:
-        metadata = MediaService.read(payload.pid)
-        # TODO fetch file content with storage_util
-        file = 'the_fetched_file'
-        return DownloadSchemaOutput(metadata=metadata, file=file)
+        mediadata = MediaService.read(payload.pid)
+        store_config = mediadata.store_config
+        match mediadata.store_config.type:
+            case StoreConfig.FILESYSTEMSTORE:
+                with storage.fs.FilesystemStore(store_config.bucket) as store:
+                    file = store.get(mediadata.store_key)
+            case StoreConfig.BUCKETSTORE:
+                s3_params = store_config.s3_params
+                s3_session = storage.s3.aiobotocore.session.get_session()
+                S3_CLIENT_ARGS = dict(
+                    endpoint_url=s3_params.url,
+                    aws_access_key_id=s3_params.access_key,
+                    aws_secret_access_key=s3_params.secret_key,  # todo, is this always available?
+                )
+                with s3_session.create_client('s3', **S3_CLIENT_ARGS) as s3client:
+                    with storage.s3.BucketStore(s3client, store_config.bucket) as store:
+                        file = store.get(mediadata.store_key)
+            case StoreConfig.SQLITESTORE:
+                with storage.db.SqliteStore(store_config.bucket) as store:
+                    file = store.put(mediadata.store_key)
+        # TODO serialise mediadata, filecontent -> file
+        return DownloadSchemaOutput(metadata=mediadata, file=file)
 
     @staticmethod
     def download_link(payload: DownloadSchemaInput) -> DownloadSchemaOutput|DownloadError:
